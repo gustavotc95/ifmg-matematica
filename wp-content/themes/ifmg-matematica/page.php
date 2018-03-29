8b8df83d3879f72e103c436625f583c8b4897288
Gustavo Castro <gustavotc95@gmail.com>
2018-03-28 23:06:47 -0300

    Site do curso de Matemática IFMG - campus Formiga

---
+++ wp-content/themes/ifmg-matematica/page.php
@@ -0,0 +1,32 @@
+<?php get_header();?>
+	<div class="limpa"></div>
+
+	<div id="single">
+		<div class="wrapper">
+			<div id="single_content">
+
+				<section id="sidebar">
+					<button class="sidebar-toggle corFundo"><span class="dashicons dashicons-arrow-left-alt2"></span></button>
+					<ul>
+						<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-1') ) : ?>
+						<?php endif; ?>
+					</ul>
+				</section>
+
+				<section id="single_post">
+					<div class="content_header corFundo"><?php the_title() ?></div>
+					<article id="single_post_inside">
+							<p>
+								<?php the_content(); ?>
+							</p>
+
+							<div class="limpa"></div>
+					</article>
+				</section>
+
+				<div class="limpa"></div>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer();?>
